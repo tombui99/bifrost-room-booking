cc1f55ddec5618f768c56a73436b6b3554478aec
tombui99 <tom.quang.bui@gmail.com>
2026-01-06 15:11:12 +0700

    chore: fix default route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Bookings } from './features/bookings/bookings';
 import { Rooms } from './features/rooms/rooms';
 import { MainLayoutComponent } from './main-layout';
 import { Dashboard } from './features/dashboard/dashboard';
+import { Login } from './auth/components/login';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: Dashboard, title: 'Dashboard' },
       { path: 'bookings', component: Bookings, title: 'Đặt Phòng' },
       { path: 'rooms', component: Rooms, title: 'Quản Lý Phòng' },
@@ -18,6 +20,6 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./auth/components/login').then((m) => m.Login),
+    component: Login,
   },
 ];
